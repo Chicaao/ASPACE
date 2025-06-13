37f8a3d82ea8548aabe2d33b1c823ff6db1f3d09
Francisco Dias de Bessa Jr <133795397+Chicaao@users.noreply.github.com>
2025-06-13 12:01:31 -0300

    correções de erros

--- js/projects.js
+++ js/projects.js
@@ -1,23 +1,26 @@
-const swiperAgricultura = new Swiper('.swiper-agricultura', {
-    loop: true,
-    pagination: {
-        el:'.swiper-pagination-agricultura',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next-agricultura',
-        prevEl: 'swiper-button-prev-agricultura',
-    },
-});
+document.addEventListener('DOMContentLoaded', function() {
 
-const swiperMeliponicultura = new Swiper('.swiper-meliponicultura', {
-    loop: true,
-    pagination: {
-        el:'.swiper-pagination-meliponicultura',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next-meliponicultura',
-        prevEl: 'swiper-button-prev-meliponicultura',
-    },
+    const swiperAgricultura = new Swiper('.swiper-agricultura', {
+        loop: true,
+        pagination: {
+            el:'.swiper-pagination-agricultura',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next-agricultura',
+            prevEl: 'swiper-button-prev-agricultura',
+        },
+    });
+
+    const swiperMeliponicultura = new Swiper('.swiper-meliponicultura', {
+        loop: true,
+        pagination: {
+            el:'.swiper-pagination-meliponicultura',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next-meliponicultura',
+            prevEl: 'swiper-button-prev-meliponicultura',
+        },
+    });
 });
