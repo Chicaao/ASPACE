027de2fcfff7ff5d400073486120e101ca1e1c5c
Francisco Dias de Bessa Jr <133795397+Chicaao@users.noreply.github.com>
2025-06-13 11:34:18 -0300

    Carrossel meliponicultura

--- js/projects.js
+++ js/projects.js
@@ -1,11 +1,23 @@
-const swiper = new Swiper('.swiper', {
+const swiperAgricultura = new Swiper('.swiper-agricultura', {
     loop: true,
     pagination: {
-        el:'.swiper-pagination',
+        el:'.swiper-pagination-agricultura',
         clickable: true,
     },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: 'swiper-button-prev',
+        nextEl: '.swiper-button-next-agricultura',
+        prevEl: 'swiper-button-prev-agricultura',
+    },
+});
+
+const swiperMeliponicultura = new Swiper('.swiper-meliponicultura', {
+    loop: true,
+    pagination: {
+        el:'.swiper-pagination-meliponicultura',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next-meliponicultura',
+        prevEl: 'swiper-button-prev-meliponicultura',
     },
 });
